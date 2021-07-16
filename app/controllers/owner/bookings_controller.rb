457b3367e1ea593e16857c0a893306c08720254a
Maelle Chancerelle <maellechancerelle@gmail.com>
2021-07-16 18:53:41 +0200

    calcul de prix et method accept

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -4,4 +4,11 @@ class Owner::BookingsController < ApplicationController
     @bookings = current_user.bookings.order(created_at: :desc)
     @booked = Booking.joins(:plant).where(plants: {user: current_user}).order(created_at: :desc)
   end
+
+  def accept_booking
+    @booking = Booking.find(params[:id])
+    @booking.accept
+    @booking.save
+    redirect_to owner_bookings_path
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,15 +5,15 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
 
   def set_price
-    self.days = (end_date - start_date).to_i
-    self.price = days * plant.price
+    days = end_date.to_date - start_date.to_date
+    self.total_price = days * plant.price_per_day
   end
 
   def accept
-    self.accepted = "accepted"
+    self.status = "accepted"
   end
 
   def refuse
-    self.accepted = "refused"
+    self.status = "refused"
   end
 end
